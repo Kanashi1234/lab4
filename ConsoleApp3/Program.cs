78effffb1e0bd6fc42608e441529cfba861603c6
Kanashi1234 <162579974+Kanashi1234@users.noreply.github.com>
2024-04-02 13:30:09 +0300

    lab4

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Dictionary<string, int> players = new Dictionary<string, int>();
+
+
+        Console.WriteLine("Введiть iм'я гравця та його бали через пробiл (наприклад, 'Гравець1 100'):");
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write($"Гравець {i + 1}: ");
+            string[] input = Console.ReadLine().Split(' ');
+            if (input.Length == 2 && int.TryParse(input[1], out int score))
+            {
+                players[input[0]] = score;
+            }
+            else
+            {
+                Console.WriteLine("Неправильний формат вводу. Спробуйте ще раз.");
+                i--;
+            }
+        }
+
+
+        var topPlayers = players.OrderByDescending(pair => pair.Value).Take(3);
+
+
+        Console.WriteLine("\nТоп 3 гравцi за сумою балiв:");
+        foreach (var player in topPlayers)
+        {
+            Console.WriteLine($"{player.Key}: {player.Value} балiв");
+        }
+    }
+}
